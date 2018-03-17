f7f372dfdc0cd236204d4391d0bd5c3315c002d6
ChristianR <christian.rogowski@yahoo.pl>
2018-03-17 14:15:23 +0100

    App start server

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,9 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.ServerAddress;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 
 import java.util.Arrays;
@@ -12,15 +15,20 @@ public class App {
 
 
         String user = "sda";
-        String database = "admin";
+        String databaseName = "admin";
         char[] password = "sda".toCharArray();
 
-        MongoCredential credential = MongoCredential.createCredential(user, database, password);
+        MongoCredential credential = MongoCredential.createCredential(user, databaseName, password);
 
         MongoClientOptions options = MongoClientOptions.builder().sslEnabled(true).build();
 
         MongoClient mongoClient = new MongoClient(new ServerAddress("cluster0-shard-00-00-eos78.mongodb.net", 27017),
                 Arrays.asList(credential),
                 options);
+
+        MongoDatabase database = mongoClient.getDatabase("admin");
+        MongoCollection<Document> coll = database.getCollection("restaurants");
+
+        mongoClient.close();
     }
 }
